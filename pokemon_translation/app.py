8706a8f6a9f0444b7dda2f11d22bc3a9d7b1679e
CASL0 <gin.0046.822@gmail.com>
2023-01-29 13:35:36 +0900

    WikiからCSVを生成し、アップロードするまで一気通貫で行う関数を作成

--- pokemon_translation/app.py
+++ pokemon_translation/app.py
@@ -2,29 +2,38 @@
 ポケモンの外国語名辞書（CSV形式）を作成します
 """
 
-import sys
 import argparse
+import logging
 from requests.exceptions import Timeout, RequestException
 from pokemon_csv import pokemon_wiki_to_csv
-from consts import CSV_FILE_NAME
+from consts import CSV_FILE_NAME, AWS_S3_BUCKET
+from utils import s3_upload
 
 
 def main():
     """wiki/ポケモンの外国語名一覧を解析し、CSV出力します"""
     parser = argparse.ArgumentParser(description="wiki/ポケモンの外国語名一覧から翻訳します")
-    parser.add_argument("--csv", help="出力するCSVファイル名", type=str)
+    parser.add_argument("--csv", help="出力するCSVファイル名", type=str, default=CSV_FILE_NAME)
     args = parser.parse_args()
+    update_csv(args.csv)
+
+
+def update_csv(csv_file_name: str) -> bool:
+    """
+    S3の翻訳ファイルを更新します
+
+    Args:
+        csv_file_name (str): アップロードするCSVファイル名
+    Returns
+        bool: 成功の場合True、失敗の場合False
+    """
     try:
-        if args.csv is None:
-            pokemon_wiki_to_csv(CSV_FILE_NAME)
-        else:
-            pokemon_wiki_to_csv(args.csv)
-    except Timeout:
-        print("request timeout")
-        sys.exit(1)
-    except RequestException as error:
-        print(f"requests failed: {error}")
-        sys.exit(1)
+        pokemon_wiki_to_csv(csv_file_name)
+        s3_upload(csv_file_name, AWS_S3_BUCKET, csv_file_name)
+        return True
+    except (Timeout, RequestException) as error:
+        logging.error("requests failed: %s", error)
+        return False
 
 
 # エントリーポイント
