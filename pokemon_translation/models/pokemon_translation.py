ab62aa86a4b10cbea1e2275384c76464d4253132
CASL0 <gin.0046.822@gmail.com>
2023-01-29 23:18:17 +0900

    dataclasses-jsonを追加し、翻訳情報のJSON文字列とdataclass変換

--- pokemon_translation/models/pokemon_translation.py
+++ pokemon_translation/models/pokemon_translation.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 
 
+@dataclass_json
 @dataclass
 class PokemonTranslation:
+    """ポケモン翻訳エンティティ"""
+
     id: str
     jpn: str
     eng: str
