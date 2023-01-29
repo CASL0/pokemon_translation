1e4a9caace2f4f1561f54f3ce0cd535d1bd9db76
CASL0 <gin.0046.822@gmail.com>
2023-01-29 23:18:54 +0900

    翻訳テーブル用のリポジトリを追加

---
+++ pokemon_translation/repository/translation_repository.py
@@ -0,0 +1,31 @@
+"""
+翻訳テーブルのリポジトリ層
+"""
+from typing import Optional
+from utils import s3_select
+from consts import AWS_S3_BUCKET
+from models import PokemonTranslation
+
+
+class TranslationRepository:
+    """
+    翻訳テーブルのCRUD操作を実行するリポジトリ
+    """
+
+    def __init__(self, s3_object_key: str) -> Optional[PokemonTranslation]:
+        self.s3_object_key = s3_object_key
+
+    def find_by_name(self, name: str):
+        """
+        ポケモン名から翻訳情報を取得します
+
+        Args:
+            name (str): ポケモンの名前
+        """
+        query = f"SELECT * FROM s3object WHERE jpn = '{ name }' OR eng = '{ name }' OR deu = '{ name }' OR fra = '{ name }' OR kor = '{ name }' OR chs = '{ name }' OR cht = '{ name }'"
+        result = s3_select(AWS_S3_BUCKET, self.s3_object_key, query)
+
+        if result is None:
+            return None
+
+        return PokemonTranslation.from_json(result)
