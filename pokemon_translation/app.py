a35ed73c254254b9b9f2823b53ffe37dfc2f1edf
CASL0 <gin.0046.822@gmail.com>
2023-01-30 20:08:11 +0900

    flask runを追加

--- pokemon_translation/app.py
+++ pokemon_translation/app.py
@@ -5,9 +5,12 @@
 import argparse
 import logging
 from requests.exceptions import Timeout, RequestException
+from flask import Flask
 from pokemon_csv import pokemon_wiki_to_csv
 from consts import CSV_FILE_NAME, AWS_S3_BUCKET
 from utils import s3_upload
+from controller import translation_bp, handle_bad_request
+from werkzeug.exceptions import BadRequest
 
 
 def main():
@@ -16,6 +19,11 @@ def main():
     parser.add_argument("--csv", help="出力するCSVファイル名", type=str, default=CSV_FILE_NAME)
     args = parser.parse_args()
     update_csv(args.csv)
+
+    app = Flask(__name__)
+    app.register_error_handler(BadRequest, handle_bad_request)
+    app.register_blueprint(translation_bp, url_prefix="/v1")
+    app.run(host="0.0.0.0", port=5000)
 
 
 def update_csv(csv_file_name: str) -> bool:
